48dc1311b748e0a9056b3ca73e62af32ab799034
MURAOKA Taro <koron.kaoriya@gmail.com>
2011-04-30 01:53:15 +0900

    separate files

--- online-update.py
+++ online-update.py
@@ -10,6 +10,7 @@ import logging
 import os
 import time
 from zipfile import ZipFile
+from downloader import Downloader
 
 URLROOT = 'http://files.kaoriya.net/vim/'
 VIMZIP = 'vim73-kaoriya-win64.zip'
@@ -23,71 +24,6 @@ UPDATE_STATUS_UPDATED   = 1
 UPDATE_STATUS_ERROR     = 2
 
 logging.basicConfig(level=logging.INFO)
-
-class Downloader:
-
-    RESULT_NOTMODIFIED  = 0
-    RESULT_DOWNLOADED   = 1
-    RESULT_ERROR        = 2
-
-    def __init__(self, inUrl, outPath, modifiedTime=0):
-        self.inUrl = inUrl
-        self.parsedUrl = urlparse(self.inUrl)
-        self.outPath = outPath
-        self.connection = None
-        self.response = None
-        self.modifiedTime = modifiedTime
-
-    def download(self):
-        response = self.__getReponse()
-        if not response:
-            logging.warning('server failure: no response')
-            return Downloader.RESULT_ERROR
-        elif response.status == 304:
-            return Downloader.RESULT_NOTMODIFIED
-        elif response.status == 200:
-            f = open(self.outPath, 'wb')
-            try:
-                f.write(response.read())
-                return Downloader.RESULT_DOWNLOADED
-            finally:
-                f.close()
-        else:
-            logging.warning('server failure: %d %s', \
-                    response.status, response.reason)
-            return Downloader.RESULT_ERROR
-
-    def __getConnection(self):
-        if not self.connection:
-            self.connection = httplib.HTTPConnection(self.parsedUrl[1])
-            #self.connection.set_debuglevel(1)
-        return self.connection
-
-    def __getHeader(self):
-        if self.modifiedTime == 0:
-            return {}
-        else:
-            # FIXME: 9 * 3600 must be got from timezone.
-            dt = datetime.fromtimestamp(self.modifiedTime - 9 * 3600)
-            since = dt.strftime('%a, %d %b %Y %H:%M:%S GMT')
-            return { 'If-Modified-Since': since }
-
-    def __getReponse(self):
-        if not self.response:
-            connection = self.__getConnection()
-            if not connection:
-                logging.warning('server failure: no connection')
-                return None
-            else:
-                header = self.__getHeader()
-                connection.request('GET', self.parsedUrl[2], None, header)
-                self.response = connection.getresponse()
-        return self.response
-
-    def close(self):
-        if self.connection:
-            self.connection.close()
-            self.connection = None
 
 """
 If-Modified-SinceをもつHTTPヘッダーを生成する.
