7b4a451c97d3bc8d382b060546b9159111da3ff3
MURAOKA Taro <koron.kaoriya@gmail.com>
2011-04-30 15:06:10 +0900

    change environment value to check cpu arch

--- vim-online-update.py
+++ vim-online-update.py
@@ -15,7 +15,7 @@ def __detectArch(rootdir):
     if os.path.exists(exe):
         arch = pe32.detectArch(exe)
     else:
-        machtype = os.environ.get('MACHTYPE')
+        machtype = os.environ.get('PROCESSOR_ARCHITECTURE')
         if machtype == 'X86':
             arch = pe32.ARCH_WIN32
         elif machtype == 'AMD64':
